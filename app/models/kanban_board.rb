c276ca5101cc803c77b4efd356691a755f6ee0a3
André Durão <andreluisdurao@gmail.com>
2015-10-03 17:04:53 -0300

    Adding first model tests

--- app/models/kanban_board.rb
+++ app/models/kanban_board.rb
@@ -1,2 +1,19 @@
-class KanbanBoard < ActiveRecord::Base
+class KanbanBoard < ActiveRecord::Base  
+  DEFAULT_COLUMNS = %i(todo in_progress to_verify done)
+
+  # Associations
+
+  # Scopes
+
+  # Validations
+  validates :title, :description, presence: true
+
+  # private
+
+  # public
+
+  def default_columns
+    DEFAULT_COLUMNS
+  end
+
 end
