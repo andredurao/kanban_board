9846caffb2107ea6449e2fd9f6bab2c20c03a40c
André Durão <andreluisdurao@gmail.com>
2015-10-03 18:48:46 -0300

    Adding devise model User

--- app/models/kanban_board.rb
+++ app/models/kanban_board.rb
@@ -5,6 +5,8 @@ class KanbanBoard < ActiveRecord::Base
   has_many :tasks
 
   # Scopes
+  scope :public_boards,  -> { where(public_visible: true) }
+  scope :private_boards, -> { where(public_visible: false) }
 
   # Validations
   validates :title, :description, presence: true
