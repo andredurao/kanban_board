854ea5d7f9322470559effda7491b06794b17111
André Durão <andreluisdurao@gmail.com>
2015-10-04 15:14:23 -0300

    Adding initial task model tests

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save a task without a KanbanBoard" do
+    task = Task.new
+    task.valid?
+    assert_includes task.errors.full_messages, "Kanban board can't be blank"
+    # "Title can't be blank", "Current status can't be blank"]
+  end
+
+  test "should not save a task without a title and current status" do
+    board = KanbanBoard.new(title: "abc", description: "abcd")
+    assert board.save
+    task = board.tasks.new
+    task.valid?
+    assert_includes task.errors.full_messages, "Title can't be blank"
+    assert_includes task.errors.full_messages, "Current status can't be blank"
+  end
 end
