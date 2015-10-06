f77046b042d56c3dfd0eb1c7deb74d216d44a654
André Durão <andreluisdurao@gmail.com>
2015-10-06 20:56:57 -0300

    Adding ORDER BY POSITION to display the tasks on the same ordered positions

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,7 +5,7 @@ class Task < ActiveRecord::Base
   belongs_to :kanban_board
 
   # Scopes
-  scope :of_status, ->(status) { where("current_status = ?", status) }
+  scope :of_status, ->(status) { where("current_status = ?", status).order(:position) }
 
   # Validations
   validates :kanban_board_id, :title, :current_status, :priority, presence: true
